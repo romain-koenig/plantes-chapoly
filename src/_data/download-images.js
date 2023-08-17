8423aeda94b8c9d8285792eb0990b84483a81e3b
Romain KOENIG <romain@kromatic.fr>
2023-08-17 14:26:27 +0200

    Centralisation du fetch dans plantes.js

--- src/_data/download-images.js
+++ src/_data/download-images.js
@@ -1,75 +1,49 @@
-// download-images.js
-const Airtable = require("airtable");
 const https = require("https");
 const fs = require("fs");
+const plantesData = require('./plantes.js');
 
-const { env } = require('process');
-
-require('dotenv').config();
-
-// Initialize Airtable with your API key and base ID
-const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
-const AIRTABLE_TABLE_NAME = process.env.AIRTABLE_PLANTES_TABLE_ID;
-
-console.log(`AIRTABLE_API_KEY: ${AIRTABLE_API_KEY}`);
-
-const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
-
-
-function downloadImage(imageUrl, details) {
-	const imageFilename = imageUrl.split("/").pop();
-
-	// Check if file already exists
-	if (fs.existsSync(`${IMAGE_FOLDER}/${imageFilename}.jpeg`)) {
-		//console.log(`File ${imageFilename} for ${details} already exists. Skipping download.`);
-		return;
-	}
-
-	const file = fs.createWriteStream(`${IMAGE_FOLDER}/${imageFilename}.jpeg`);
-	https.get(imageUrl, function (response) {
-		response.pipe(file);
-		//console.log(`Downloaded ${imageFilename} for ${details}`);
-	});
-}
-
-
-// Create a folder to store the images
 const IMAGE_FOLDER = "public/images/plantes";
+
+// Check if folder exists or create one
 if (!fs.existsSync(IMAGE_FOLDER)) {
 	fs.mkdirSync(IMAGE_FOLDER, { recursive: true });
 }
 
-// Fetch the records from Airtable
-base(AIRTABLE_TABLE_NAME).select().eachPage(function page(records, fetchNextPage) {
+function downloadImage(imageUrl, details) {
+	const imageFilename = imageUrl.split("/").pop().concat(".jpeg");
 
-	records.forEach(function (record) {
-		const pics = record.get("Pics") || [];
+	// Check if file already exists
+	if (fs.existsSync(`${IMAGE_FOLDER}/${imageFilename}`)) {
+		console.log(`File ${imageFilename} for ${details} already exists. Skipping download.`);
+		return;
+	}
 
-		const plantName = record.get("Name");
+	const file = fs.createWriteStream(`${IMAGE_FOLDER}/${imageFilename}`);
+	https.get(imageUrl, function (response) {
+		response.pipe(file);
+	});
+}
+
+async function main() {
+	const plantes = await plantesData();
+
+	plantes.forEach(plante => {
+		const plantName = plante.name;
 		// console.log(`Processing ${plantName}`);
 
-		pics.forEach(pic => {
+		plante.photo_infos.forEach(pic => {
 			// Download original size
-			downloadImage(pic.url, `${plantName} (full)`);
+			downloadImage(pic.full_photo_url, `${plantName} (full)`);
 
 			// Download small size
-			if (pic.thumbnails && pic.thumbnails.small) {
-				downloadImage(pic.thumbnails.small.url, `${plantName} (small)`);
-			}
+			downloadImage(pic.small_photo_url, `${plantName} (small)`);
 
 			// Download large size
-			if (pic.thumbnails && pic.thumbnails.large) {
-				downloadImage(pic.thumbnails.large.url, `${plantName} (large)`);
-			}
+			downloadImage(pic.large_photo_url, `${plantName} (large)`);
 		});
 	});
+}
 
-
-
-	// Fetch next page of records if there are more pages
-	fetchNextPage();
-
-}, function done(err) {
-	if (err) { console.error(err); return; }
+main().catch(err => {
+	console.error(err);
 });
